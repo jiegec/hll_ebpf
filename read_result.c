ecf973c5c849890069715a8c72c852e99f62b570
Jiajie Chen <jiegec@qq.com>
2018-06-17 11:01:24 +0800

    Add sample command

--- read_result.c
+++ read_result.c
@@ -1,21 +1,21 @@
 /**
  * Copyright (C) 2018 Jiajie Chen
- * 
+ *
  * This file is part of hll_ebpf.
- * 
+ *
  * hll_ebpf is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * hll_ebpf is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with hll_ebpf.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 #include <linux/bpf.h>
@@ -24,6 +24,7 @@
 #define _GNU_SOURCE
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
@@ -64,19 +65,39 @@ int bpf_map_lookup_elem(int fd, const void *key, void *value) {
   return sys_bpf(BPF_MAP_LOOKUP_ELEM, &attr, sizeof(attr));
 }
 
+int bpf_map_update_elem(int fd, void *key, void *value,
+                        unsigned long long flags) {
+  union bpf_attr attr = {
+      .map_fd = fd,
+      .key = ptr_to_u64(key),
+      .value = ptr_to_u64(value),
+      .flags = flags,
+  };
+
+  return sys_bpf(BPF_MAP_UPDATE_ELEM, &attr, sizeof(attr));
+}
+
+const static int b = 6;
+const static int m = 1 << b;
+
+void clear_file(const char *file) {
+  int fd = bpf_obj_get(file);
+  for (unsigned long i = 0; i < m; i++) {
+    unsigned long value[128] = {0};
+    bpf_map_update_elem(fd, &i, &value, 0);
+  }
+}
+
 void read_file(const char *file) {
   int fd = bpf_obj_get(file);
-  const static int b = 6;
-  const static int m = 1 << b;
   int M[m] = {0};
   int V = 0;
   double sum = 0;
   int nprocs = get_nprocs();
-  for (unsigned long i = 0; i < m; i++)
-  {
+  for (unsigned long i = 0; i < m; i++) {
     unsigned long value[128] = {0};
     bpf_map_lookup_elem(fd, &i, &value);
-    for (int j = 0; j < nprocs;j++) {
+    for (int j = 0; j < nprocs; j++) {
       M[i] = M[i] > value[j] ? M[i] : value[j];
     }
     if (M[i] == 0)
@@ -94,8 +115,33 @@ void read_file(const char *file) {
   printf("%ld\n", lround(E));
 }
 
-int main() {
+void clear_all() {
+  clear_file("/sys/fs/bpf/tc/globals/hll_ebpf_in_saddr");
+  clear_file("/sys/fs/bpf/tc/globals/hll_ebpf_out_daddr");
+}
+
+void read_all() {
   read_file("/sys/fs/bpf/tc/globals/hll_ebpf_in_saddr");
   read_file("/sys/fs/bpf/tc/globals/hll_ebpf_out_daddr");
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2 || strcmp(argv[1], "read") == 0) {
+    printf("Reading hll counter (read, write):\n");
+    read_all();
+  } else if (argc >= 2 && strcmp(argv[1], "sample") == 0) {
+    printf("Clearing hll counter.\n");
+    clear_all();
+    int time = 10;
+    if (argc == 3) {
+      time = atoi(argv[2]);
+    }
+    printf("Waiting for %d seconds\n", time);
+    sleep(time);
+    printf("Reading hll counter (read, write):\n");
+    read_all();
+  } else {
+    printf("Usage: read_result [read|sample [nsecs]]\n");
+  }
   return 0;
 }
